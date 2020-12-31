681ee02d43f63b119ec074321798af9f07344094
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-01 00:35:06 +0800

    change log file name

--- CheckNet.ps1
+++ CheckNet.ps1
@@ -1,10 +1,6 @@
 ﻿cd $PSScriptRoot
 
-$x=python3 cli.py net checklogin -u zhouyc19
-echo $x
-$x > checklogin.txt
-if($x -eq 'not login'){
-    # $pswd = cat .\pswd.txt
-    $pswd = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String((cat .\pswd.b64)))
-    $pswd | python .\cli.py net login -u zhouyc19
-}
+# $pswd = cat .\pswd.txt
+$pswd = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String((cat .\pswd.b64)))
+$pswd | python .\cli.py net login -u zhouyc19
+python3 cli.py net checklogin -u zhouyc19 > log.txt
--- CheckNet.sh
+++ CheckNet.sh
@@ -2,5 +2,4 @@
 cd "$(dirname "$0")"
 
 base64 -d pswd.b64 | python3 cli.py net login -u zhouyc19
-python3 cli.py net checklogin -u zhouyc19 > checklogin.txt
-
+python3 cli.py net checklogin -u zhouyc19 > log.txt
