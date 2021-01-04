0189571026a08917e91b6c04422dc48a6669d5dd
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-04 22:22:08 +0800

    change net to auth4

--- CheckNet.ps1
+++ CheckNet.ps1
@@ -2,5 +2,5 @@
 
 # $pswd = cat .\pswd.txt
 $pswd = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String((cat .\pswd.b64)))
-$pswd | python .\cli.py net login -u zhouyc19
-python3 cli.py net checklogin -u zhouyc19 > log.txt
+$pswd | python .\cli.py auth4 login -u zhouyc19
+python3 cli.py auth4 checklogin -u zhouyc19 > log.txt
--- CheckNet.sh
+++ CheckNet.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 
-base64 -d pswd.b64 | python3 cli.py net login -u zhouyc19
-python3 cli.py net checklogin -u zhouyc19 > log.txt
+base64 -d pswd.b64 | python3 cli.py auth4 login -u zhouyc19
+python3 cli.py auth4 checklogin -u zhouyc19 > log.txt
