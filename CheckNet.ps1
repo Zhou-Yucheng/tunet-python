ee5ecfba0b23eac7e23b64d0e80da1e4ce78aac4
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-05 15:20:33 +0800

    fix: add --net

--- CheckNet.ps1
+++ CheckNet.ps1
@@ -1,6 +1,7 @@
 ﻿cd $PSScriptRoot
 
+$username=cat .\username.txt
 # $pswd = cat .\pswd.txt
 $pswd = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String((cat .\pswd.b64)))
-$pswd | python .\cli.py auth4 login -u zhouyc19
-python3 cli.py auth4 checklogin -u zhouyc19 > log.txt
+$pswd | python3 .\cli.py auth4 login --net -u $username
+python3 cli.py auth4 checklogin -u $username > log.txt
--- CheckNet.sh
+++ CheckNet.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 
-base64 -d pswd.b64 | python3 cli.py auth4 login -u zhouyc19
-python3 cli.py auth4 checklogin -u zhouyc19 > log.txt
+username=$(cat username.txt)
+# cat pswd.txt | python3 cli.py auth4 login --net -u $username
+base64 -d pswd.b64 | python3 cli.py auth4 login --net -u $username
+python3 cli.py auth4 checklogin -u $username > log.txt
