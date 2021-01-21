19ce443d340514731fa71c27518dcef0c82665e5
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-21 15:45:10 +0800

    ignore bak/

--- CheckNet.ps1
+++
@@ -1,7 +0,0 @@
-﻿cd $PSScriptRoot
-
-$username=cat .\username.txt
-# $pswd = cat .\pswd.txt
-$pswd = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String((cat .\pswd.b64)))
-$pswd | python3 .\cli.py auth4 login --net -u $username
-python3 cli.py auth4 checklogin -u $username > log.txt
