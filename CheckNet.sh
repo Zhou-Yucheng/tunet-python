1c2541bb0ad396a20d19e31d34961805618cc645
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-21 15:47:29 +0800

    del CheckNet.sh

--- CheckNet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-
-username=$(cat username.txt)
-# cat pswd.txt | python3 cli.py auth4 login --net -u $username
-base64 -d pswd.b64 | python3 cli.py auth4 login --net -u $username
-python3 cli.py auth4 checklogin -u $username > log.txt
