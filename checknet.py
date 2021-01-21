c587ef647b1ad82d0f8db1b9be111f6e9c111184
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-21 15:59:05 +0800

    update style

--- checknet.py
+++ checknet.py
@@ -7,7 +7,7 @@ import tunet
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-r', '--relogin', action='store_true', help='logout and re-login')
+parser.add_argument('-r', '--relogin', action='store_true', help='logout and relogin')
 args = parser.parse_args()
 
 
@@ -17,12 +17,14 @@ if __name__ == "__main__":
     password=base64.b64decode(Path(dir / 'pswd.b64').read_text())
 
     if args.relogin:
+        print('logout and relogin...')
         tunet.auth4.logout(username)
         tunet.auth4.login(username, password, net=True)
 
-    if not tunet.net.checklogin():
+    res=tunet.net.checklogin()
+    if not res:
         tunet.auth4.login(username, password, net=True)
+        res=tunet.net.checklogin()
 
-    check=tunet.net.checklogin()
-    print(check)
-    Path(dir / 'log.txt').write_text(str(check)+'\n')
+    print(res)
+    Path(dir / 'log.txt').write_text(str(res)+'\n')
