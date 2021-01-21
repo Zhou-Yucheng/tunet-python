e5ea2fffbbbbc8f5f3461bf0b42cf683a5a0d05d
Zhou-Yucheng <zhouyucheng98@outlook.com>
2021-01-21 20:28:15 +0800

    support -relogin switch

---
+++ checknet.ps1
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/pwsh
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [switch]$relogin
+)
+
+cd $PSScriptRoot
+$username=cat username.txt
+$pswd = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String((cat pswd.b64)))
+# $pswd = cat pswd.txt
+
+if($relogin){
+    echo 'logout and relogin'
+    python3 cli.py auth4 logout -u $username
+}
+$pswd | python3 cli.py auth4 login --net -u $username
+python3 cli.py auth4 checklogin -u $username | tee log.txt
